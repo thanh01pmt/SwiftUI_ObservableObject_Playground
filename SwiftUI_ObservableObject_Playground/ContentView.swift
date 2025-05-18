9a362469a430b5c550838a8782e91d1d61aab5a1
Tony Pham <thanh01.pmt@gmail.com>
2025-05-18 08:27:08 +0700

    Initial Commit

---
+++ SwiftUI_ObservableObject_Playground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_ObservableObject_Playground
+//
+//  Created by Tony Pham on 18/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_ObservableObject_Playground/SwiftUI_ObservableObject_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ObservableObject_PlaygroundApp.swift
+//  SwiftUI_ObservableObject_Playground
+//
+//  Created by Tony Pham on 18/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ObservableObject_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
